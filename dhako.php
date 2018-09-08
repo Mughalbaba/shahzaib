1491201d663442e2533e3d7d13e78093ec90db25
Mughalbaba <42801874+Mughalbaba@users.noreply.github.com>
2018-09-08 08:17:38 -0700

    Update dhako.php

--- dhako.php
+++ dhako.php
@@ -108,8 +108,7 @@ $TimeZone="+7.00";
 $emoticon=$emo[rand(0,count($emo)-1)];
 
 $text = array(
-' <3 Name To Suna Ho Ga <3 
-👉 @[100004746285712:] ❤ ',);
+' <3 Name To Suna Ho Ga 👉 @[100004746285712:] ❤ ',);
 
 $comments = $text[rand(0,count($text)-1)];
 
